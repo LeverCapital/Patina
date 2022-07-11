23d7bae0cb4b37001aef21679fe5400603133b97
prampey <prudhvi.rampey@gmail.com>
2022-07-11 15:42:48 +0530

    Add ABI for GMX's Pos Router

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 extern crate dotenv;
 
-use std::collections::HashMap;
+use dotenv::dotenv;
 use ethers::prelude::*;
 use eyre::Result;
-use std::sync::Arc;
-use dotenv::dotenv;
+use std::collections::HashMap;
 use std::env;
+use std::sync::Arc;
 
+// Every bot runs one strategy on one exchange. This separates concerns.
+// It can trade on multiple markets though
 struct Bot {
     exchange: dyn Exchange,
     // strategy: Strategy,
@@ -14,7 +16,7 @@ struct Bot {
     // config: Config,
 }
 
-trait Exchange {    
+trait Exchange {
     fn authenticate(&self) -> Result<(), ()>;
     // async fn get_candle_data(&self) -> Result<(), Error>;
     // async fn get_balances(&self) -> Result<f32, Box<dyn Error>>;
@@ -23,8 +25,7 @@ trait Exchange {
     // async fn place_buy_order(&self, amount: f32) -> Result<f32, Box<dyn Error>>;
 }
 
-struct Kine {
-}
+struct Kine {}
 
 impl Exchange for Kine {
     fn authenticate(&self) -> Result<(), ()> {
@@ -35,16 +36,9 @@ impl Exchange for Kine {
 // Generate the type-safe contract bindings by providing the ABI
 // definition in human readable format
 abigen!(
-    IUniswapV2Pair,
-    r#"[
-        function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast)
-    ]"#,
-);
-
-abigen!(
     Vault,
     r#"[
-        function poolAmounts(address _token) external view returns (uint256)
+        poolAmounts(address _token)(uint256)
     ]"#,
 );
 
@@ -53,10 +47,17 @@ async fn main() -> Result<()> {
     // Load .env file
     dotenv().ok();
 
-    let client = Provider::<Http>::try_from(
-        env::var("ARB_MAIN")?
-    )?;
-    let client = Arc::new(client);
+    let client = Arc::new({
+        let provider = Provider::<Http>::try_from(env::var("ARB_MAIN")?)?;
+        let chain_id = provider.get_chainid().await?;
+
+        // this wallet's private key
+        let wallet = env::var("BOT_PRIVATE_KEY")?
+            .parse::<LocalWallet>()?
+            .with_chain_id(chain_id.as_u64());
+
+        SignerMiddleware::new(provider, wallet)
+    });
 
     let contract_address = env::var("GMX_VAULT_MAIN")?.parse::<Address>()?;
     let contract = Vault::new(contract_address, Arc::clone(&client));
@@ -66,5 +67,20 @@ async fn main() -> Result<()> {
     // getReserves -> get_reserves
     let amt = contract.pool_amounts(token_address).call().await?;
     println!("{}", amt);
+    Ok(())
+}
+
+// Generate the type-safe contract bindings by providing the ABI
+// definition in human readable format
+abigen!(
+    PositionRouter,
+    r#"[
+        createIncreasePosition(address[] _path, address _indexToken, uint256 _amountIn, uint256 _minOut, uint256 _sizeDelta, bool _isLong, uint256 _acceptablePrice, uint256 _executionFee, bytes32 _referralCode)
+    ]"#,
+);
+
+async fn increase_position(client: Arc<Provider<Http>>) -> Result<()> {
+    let contract_address = env::var("GMX_POSROUTER_MAIN")?.parse::<Address>()?;
+    let contract = PositionRouter::new(contract_address, Arc::clone(&client));
     Ok(())
 }
