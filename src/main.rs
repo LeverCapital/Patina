d500ebd2ee9bcb9741dd1391465d17701e539215
Prudhvi Rampey <prudhvi.rampey@gmail.com>
2022-12-16 22:05:35 -0500

    Can listen to createIncreaseOrder events on GMX!
    - Update dependencies

--- src/main.rs
+++ src/main.rs
@@ -3,42 +3,19 @@ extern crate dotenv;
 use dotenv::dotenv;
 use ethers::prelude::*;
 use eyre::Result;
-use std::collections::HashMap;
 use std::env;
 use std::sync::Arc;
+use ethers::{abi::AbiDecode, utils::keccak256};
+use hex::FromHex;
 
-// Every bot runs one strategy on one exchange. This separates concerns.
-// It can trade on multiple markets though
-struct Bot {
-    exchange: dyn Exchange,
-    // strategy: Strategy,
-    // markets: Market[],
-    // config: Config,
-}
-
-trait Exchange {
-    fn authenticate(&self) -> Result<(), ()>;
-    // async fn get_candle_data(&self) -> Result<(), Error>;
-    // async fn get_balances(&self) -> Result<f32, Box<dyn Error>>;
-    // async fn get_market_price(&self) -> Result<f32, Box<dyn Error>>;
-    // async fn place_sell_order(&self, amount: f32) -> Result<f32, Box<dyn Error>>;
-    // async fn place_buy_order(&self, amount: f32) -> Result<f32, Box<dyn Error>>;
-}
-
-struct Kine {}
-
-impl Exchange for Kine {
-    fn authenticate(&self) -> Result<(), ()> {
-        Ok(())
-    }
-}
-
+pub mod utils;
+// pub mod model;
 // Generate the type-safe contract bindings by providing the ABI
 // definition in human readable format
 abigen!(
-    CryptoKitty,
+    OrderBook,
     r#"[
-     isPregnant(uint256 _kittyId)(bool)
+        event CreateIncreaseOrder(address indexed account, uint256 orderIndex, address purchaseToken, uint256 purchaseTokenAmount, address collateralToken, address indexToken, uint256 sizeDelta, bool isLong, uint256 triggerPrice, bool triggerAboveThreshold, uint256 executionFee)
     ]"#,
 );
 
@@ -46,50 +23,26 @@ abigen!(
 async fn main() -> Result<()> {
     // Load .env file
     dotenv().ok();
-    // Setup bot's wallet
-    let client = Arc::new({
-        let provider = Provider::<Http>::try_from(env::var("ETH_MAIN")?)?;
-        let chain_id = provider.get_chainid().await?;
+    // Connect to Provider
+    let client = utils::create_websocket_client().await?;
 
-        // this wallet's private key
-        let wallet = env::var("BOT_PRIVATE_KEY")?
-            .parse::<LocalWallet>()?
-            .with_chain_id(chain_id.as_u64());
+    // Orderbook contract
+    let orderbook_contract = OrderBook::new(
+        "0x09f77E8A13De9a35a7231028187e9fD5DB8a2ACB".parse::<Address>()?,
+        client.clone(),
+    );
 
-        SignerMiddleware::new(provider, wallet)
-    });
-
-    let contract_address = "0x06012c8cf97BEaD5deAe237070F9587f8E7A266d".parse::<Address>()?;
-    let contract = CryptoKitty::new(contract_address, Arc::clone(&client));
-
-    let mut numPreggos = 0;
-    let supply = 2013347;
-    for n in 275495..supply {
-        let index = n.to_string().parse::<U256>()?;
-        let check = contract.is_pregnant(index).call().await?;
-        if check {
-            numPreggos += 1;
-            println!("{}", n);
-            if numPreggos == 22 {
-                break;
-            }
-        }
+    println!("Starting to listen to events....");
+    // Subscribe to CreateIncreaseOrder events
+    let events = orderbook_contract.events();
+    let mut stream = events.stream().await?.take(2);
+    while let Some(Ok(event)) = stream.next().await {
+        println!(
+            "from: {:?}",
+            event.account,
+        );
     }
 
     Ok(())
-}
-
-// Generate the type-safe contract bindings by providing the ABI
-// definition in human readable format
-abigen!(
-    PositionRouter,
-    r#"[
-        createIncreasePosition(address[] _path, address _indexToken, uint256 _amountIn, uint256 _minOut, uint256 _sizeDelta, bool _isLong, uint256 _acceptablePrice, uint256 _executionFee, bytes32 _referralCode)
-    ]"#,
-);
-
-async fn increase_position(client: Arc<Provider<Http>>) -> Result<()> {
-    let contract_address = env::var("GMX_POSROUTER_MAIN")?.parse::<Address>()?;
-    let contract = PositionRouter::new(contract_address, Arc::clone(&client));
-    Ok(())
-}
+    
+        }
